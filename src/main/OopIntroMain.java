6cfb93671dd8f181730d710e858031ae53b28a44
Cezar-Andrei <khaezar67@yahoo.com>
2022-07-20 14:34:05 +0300

    Initial commit

---
+++ src/main/OopIntroMain.java
@@ -0,0 +1,17 @@
+package main;
+
+public class OopIntroMain {
+    public static void main(String[] args) {
+
+        System.out.println("Starting the investigation...");
+
+        Person p1= new Person();
+
+        System.out.println("Investigation is still running");
+
+        Person p2= new Person();
+
+        System.out.println("Investigation is done!");
+
+    }
+}
---
+++ src/main/Person.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Person {
+
+    private int age;
+    private String name;
+    private boolean studentID;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(boolean studentID) {
+        this.studentID = studentID;
+    }
+}
